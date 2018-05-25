40d58e2e3189b83095d9c32c6b0e5fdd1f8adb1b
Jakub Vaňo <jakub.vano@cleverlance.com>
2018-05-25 17:02:43 +0200

    Initial Commit

---
+++ Stiletto/Stiletto.h
@@ -0,0 +1,19 @@
+//
+//  Stiletto.h
+//  Stiletto
+//
+//  Created by Jakub Vaňo on 25/05/2018.
+//  Copyright © 2018 SwinjectContributors. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Stiletto.
+FOUNDATION_EXPORT double StilettoVersionNumber;
+
+//! Project version string for Stiletto.
+FOUNDATION_EXPORT const unsigned char StilettoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Stiletto/PublicHeader.h>
+
+
